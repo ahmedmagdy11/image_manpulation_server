1bd0a123ef48e901b680cc93b48e656aedcfdde5
Ahmedmagdy11 <ahmed.magdy.9611@gmail.com>
2021-11-10 19:24:00 +0200

    changed waterwark function

--- function/pipeline.js
+++ function/pipeline.js
@@ -34,8 +34,17 @@ async function compress(image) {
 
 async function addWaterMark(image) {
     const newImage = getNewImagePath(image);
-    const watermark = __dirname.replace("function", "watermark.png");
-    await asyncExec(` ffmpeg -i ${image} -i ${watermark} -filter_complex "overlay=x=(main_w-overlay_w)/4:y=(main_h-overlay_h)/4" ${newImage} -y`);
+    const watermark = __dirname.replace("function", "watermark-100%.png");
+    const applyWaterMark = `ffmpeg -y \
+    -i ${image} \
+    -i  ${watermark} \
+    -filter_complex "\
+    [1][0]scale2ref=h=ow/mdar:w=iw/4[#A watermark][main_image];\
+    [#A watermark]format=argb,colorchannelmixer=aa=0.7[#B watermark transparent];\
+    [main_image][#B watermark transparent]overlay\
+    =(main_w-w)/4:(main_h-h)/2" \
+    ${newImage}`;
+    await asyncExec(applyWaterMark);
     deleteTheOldImageAndRename(image, newImage);
     return;
 }
